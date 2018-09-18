dea2d4601a87732bb4edcd1d71d38b51976feca8
Team Ninja (Emanuele) <emanuele.malfara@trovaprezzi.it>
2018-09-18 14:02:22 +0200

    verdi

--- exercise.rb
+++ exercise.rb
@@ -1,5 +1,10 @@
 class Exercise
   def shifted?(first, second)
+    0.upto(first.length) do |index|
+      part1 = first[index..first.length-1]
+      part2 = (index == 0) ? '' : first[0..index-1]
+      return true if "#{part1}#{part2}" == second
+    end
     false
   end
 end
